1d23e5925509fee2de9abb0c74cfd16600ed4a60
“Adithya <adithyasrinivas66@gmail.com>
2022-07-04 20:24:11 +0530

    my Address

---
+++ Addressbook.java
@@ -0,0 +1,24 @@
+package main.day9;
+import java.util.Scanner
+
+public class Addressbook {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private  String state;
+    private  long zip;
+    private  long phoneNumber;
+    private  String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName=firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+}
